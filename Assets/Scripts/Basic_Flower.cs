30b728c4164a60f85a100a2699c052736c2654fe
Will Cosker <wcosker@ufl.edu>
2020-10-30 16:04:43 -0400

    waterbucket added and flower healing fixed

--- Assets/Scripts/Basic_Flower.cs
+++ Assets/Scripts/Basic_Flower.cs
@@ -13,7 +13,6 @@ public class Basic_Flower : MonoBehaviour
     private float originalTime;
     private SpriteRenderer sr;
     private bool canHeal = false;//flower can only be healed when timer is at certain point
-    private bool currWatering = false;//check for watering bucket timing
 
     // Start is called before the first frame update
     void Start()
@@ -32,8 +31,8 @@ public class Basic_Flower : MonoBehaviour
 
         checkFlowerHealth();//calls function that checks flowers time til death/health
     }
-                                                                       //TODO: make this more dynamic, every flower should be able to call this method cleanly
-    void checkFlowerHealth()
+                                                                       
+    void checkFlowerHealth()//TODO: make this more dynamic, every flower should be able to call this method cleanly
     {
         if (originalTime / 2 > timeTilDeath)
         {
@@ -42,35 +41,15 @@ public class Basic_Flower : MonoBehaviour
         }
     }
 
-    public void resetTimer()//this brings the flower back to "full hp"
+    public void resetTimer()//this brings the flower back to "full hp" changes sprite and sets the flower back to not healable
     {
         timeTilDeath = originalTime;
+        sr.sprite = healthySprite;
+        canHeal = false;
     }
 
-
-    IEnumerator OnTriggerEnter2D(Collider2D col)                    //collision with player occurs
+    public bool getCanHeal()//allows other scripts to access flowers healing status
     {
-        if (col.tag == "Player" && canHeal)
-        {
-            currWatering = true;
-            //TODO: BEGIN WATERING ANIMATION HERE
-            UnityEngine.Debug.Log("Waiting...");
-            yield return new WaitForSeconds(2);
-            UnityEngine.Debug.Log("Checking!");
-            if (currWatering)
-            {
-                resetTimer();                                       //if the flower can be healed AND the player stood on it for a long enough time, it will be healed
-                sr.sprite = healthySprite;                          //replaces the sprite and resets the can heal property
-                canHeal = false;
-                                                                    //TODO: take water out of watering bucket and make a sound?
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        //TODO: cancel watering animation
-        currWatering = false;
-        UnityEngine.Debug.Log("U left");
+        return canHeal;
     }
 }
---
+++ Assets/Scripts/Water_Bucket.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Water_Bucket : MonoBehaviour
+{
+    private bool currWatering = false;
+    private float wateringTime = 2f;//this can be an upragadable skill?
+    private float countdown;
+    private int uses = 4;//get this value from other script upgrade?????
+    private Basic_Flower flower;
+
+
+    /* *************************************************************************************************************
+       * This class is for the WaterBucket Fucntionality. The triggerenter and stay see if the flower is waterable *
+       * and if it is and you are already not watering the plant, you will begin watering it.                      *
+       * The exit function checks for the flower being watered.                                                    *
+       *                                                                                                           *
+       * The update func has the countdown for the time spent watering because of timing reasons                   *
+       *                                                                                                           *
+       * The rest of the script has the uses incrementer and decrementer                                           *
+       *************************************************************************************************************
+     */
+    private void OnTriggerEnter2D(Collider2D col)//collision with flower object occurs
+    {
+        if (col.tag == "FlowerEnemy" && col.GetComponent<Basic_Flower>().getCanHeal() && uses > 0)
+        {
+            flower = col.GetComponent<Basic_Flower>();
+            countdown = wateringTime;
+            currWatering = true;
+            //TODO: BEGIN WATERING ANIMATION HERE
+            UnityEngine.Debug.Log("Begin watering... (FROM ENTER)");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)//player is already in object
+    {
+        if (!currWatering && col.tag == "FlowerEnemy" && col.GetComponent<Basic_Flower>().getCanHeal() && uses > 0)
+        {
+            flower = col.GetComponent<Basic_Flower>();
+            countdown = wateringTime;
+            currWatering = true;
+            //TODO: BEGIN WATERING ANIMATION HERE
+            UnityEngine.Debug.Log("Begin watering... (FROM STAY)");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (currWatering)//this is catching early leaving of watering
+        {
+            UnityEngine.Debug.Log("exited before watering completed!");
+            currWatering = false;
+        }
+    }
+
+    void Update()
+    {
+        if (currWatering)//checks if flower is being watered
+        {
+            countdown -= Time.deltaTime;
+            if (countdown < 0)//END WATERING CYCLE
+            {
+                //UnityEngine.Debug.Log("Flower has been healed!");
+                flower.resetTimer();
+                currWatering = false;
+                uses--;
+                //TODO: cancel water animation
+            }
+        }
+    }
+}
